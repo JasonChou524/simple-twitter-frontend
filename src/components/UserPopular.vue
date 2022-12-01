24d2173b96b585166b15cb51a59cde8f97fba801
JasonChou <swchou524@gmail.com>
2022-12-01 11:41:25 +0800

    feat: add NavTabs style

---
+++ src/components/UserPopular.vue
@@ -0,0 +1,9 @@
+<template>
+  <h3>UserPopular</h3>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,10 +1,34 @@
 <template>
-  <h1>Main</h1>
+  <div class="main-container">
+    <div class="row">
+      <nav-tabs class="col-3" />
+      <div class="middle col-6">
+        <h1>Main</h1>
+      </div>
+      <user-popular class="col-3" />
+    </div>
+  </div>
 </template>
 
 <script>
+import NavTabs from '../components/NavTabs.vue'
+import UserPopular from '../components/UserPopular.vue'
 
 export default {
-  name: 'Main'
+  name: 'Main',
+  components: {
+    NavTabs,
+    UserPopular
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+  .main-container {
+    width: 100vw;
+    height: 100vh;
+    max-width: 1400px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
