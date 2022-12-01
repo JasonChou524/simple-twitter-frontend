857d1f87fa8abb01911e1a00966e52ebc5038c84
JasonChou <swchou524@gmail.com>
2022-12-01 23:13:32 +0800

    feat: add UserPopular function and set prettier

--- src/components/UserPopular.vue
+++ src/components/UserPopular.vue
@@ -2,50 +2,428 @@
   <div class="wrap">
     <div class="popular-list">
       <div class="title">
-        <h4>推薦追隨</h4>
+        <h4>推薦跟隨</h4>
       </div>
-      <div class="user-card">
+      <div
+        v-for="user of users"
+        :key="user.id"
+        class="user-card"
+      >
         <div class="profile">
           <div class="avatar">
-            <img src="~@/assets/image/tweet-default.png" alt="" />
+            <img
+              v-if="user.avatar"
+              :src="user.avatar"
+              alt="user's avatar"
+            />
+            <img
+              v-else
+              src="~@/assets/image/tweet-default.png"
+              alt="user's avatar"
+            />
           </div>
           <div class="info">
-            <h6>Pizza Hut</h6>
-            <p>@pizzahut</p>
+            <h6>{{user.name}}</h6>
+            <p>@{{user.account}}</p>
           </div>
         </div>
-        <button class="unfollow-btn">正在追隨</button>
-      </div>
-      <div class="user-card">
-        <div class="profile">
-          <div class="avatar">
-            <img src="~@/assets/image/tweet-default.png" alt="" />
-          </div>
-          <div class="info">
-            <h6>Pizza Hut</h6>
-            <p>@pizzahut</p>
-          </div>
-        </div>
-        <button class="unfollow-btn">正在追隨</button>
-      </div>
-      <div class="user-card">
-        <div class="profile">
-          <div class="avatar">
-            <img src="~@/assets/image/tweet-default.png" alt="" />
-          </div>
-          <div class="info">
-            <h6>Pizza Hut</h6>
-            <p>@pizzahut</p>
-          </div>
-        </div>
-        <button class="follow-btn">追隨</button>
+        <button
+          v-if="user.isFollowed"
+          class="unfollow-btn"
+          @click="removeFollow(user.id)"
+        >
+          正在跟隨
+        </button>
+        <button
+          v-else
+          class="follow-btn"
+          @click="addFollow(user.id)"
+        >
+          跟隨
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+const dummyUsers = [
+  {
+    id: 24,
+    name: 'user2',
+    avatar: 'https://randomuser.me/api/portraits/women/2.jpg',
+    account: 'user2',
+    followersCount: 4,
+    Followers: [
+      {
+        id: 84,
+        name: 'user99',
+        avatar: 'https://i.imgur.com/fGVq8DU.jpeg',
+        account: 'user9',
+        Followship: {
+          id: 314,
+          followerId: 84,
+          followingId: 24,
+          createdAt: '2022-08-07T14:04:40.000Z',
+          updatedAt: '2022-08-07T14:04:40.000Z'
+        }
+      },
+      {
+        id: 64,
+        name: 'ＲＥＤ',
+        avatar: 'https://i.imgur.com/oEu32YO.jpeg',
+        account: 'asd',
+        Followship: {
+          id: 574,
+          followerId: 64,
+          followingId: 24,
+          createdAt: '2022-08-07T16:18:15.000Z',
+          updatedAt: '2022-08-07T16:18:15.000Z'
+        }
+      },
+      {
+        id: 164,
+        name: 'wjy',
+        avatar: 'https://i.imgur.com/1gLP1yi.jpeg',
+        account: 'wjy',
+        Followship: {
+          id: 734,
+          followerId: 164,
+          followingId: 24,
+          createdAt: '2022-08-10T10:21:13.000Z',
+          updatedAt: '2022-08-10T10:21:13.000Z'
+        }
+      },
+      {
+        id: 14,
+        name: 'Sandy',
+        avatar: 'https://i.imgur.com/ljf0Tla.jpeg',
+        account: 'user1',
+        Followship: {
+          id: 764,
+          followerId: 14,
+          followingId: 24,
+          createdAt: '2022-09-22T06:07:05.000Z',
+          updatedAt: '2022-09-22T06:07:05.000Z'
+        }
+      }
+    ],
+    isFollowed: true
+  },
+  {
+    id: 14,
+    name: 'Sandy',
+    avatar: 'https://i.imgur.com/ljf0Tla.jpeg',
+    account: 'user1',
+    followersCount: 3,
+    Followers: [
+      {
+        id: 64,
+        name: 'ＲＥＤ',
+        avatar: 'https://i.imgur.com/oEu32YO.jpeg',
+        account: 'asd',
+        Followship: {
+          id: 564,
+          followerId: 64,
+          followingId: 14,
+          createdAt: '2022-08-07T16:18:14.000Z',
+          updatedAt: '2022-08-07T16:18:14.000Z'
+        }
+      },
+      {
+        id: 84,
+        name: 'user99',
+        avatar: 'https://i.imgur.com/fGVq8DU.jpeg',
+        account: 'user9',
+        Followship: {
+          id: 614,
+          followerId: 84,
+          followingId: 14,
+          createdAt: '2022-08-07T16:19:37.000Z',
+          updatedAt: '2022-08-07T16:19:37.000Z'
+        }
+      },
+      {
+        id: 164,
+        name: 'wjy',
+        avatar: 'https://i.imgur.com/1gLP1yi.jpeg',
+        account: 'wjy',
+        Followship: {
+          id: 704,
+          followerId: 164,
+          followingId: 14,
+          createdAt: '2022-08-10T10:18:10.000Z',
+          updatedAt: '2022-08-10T10:18:10.000Z'
+        }
+      }
+    ],
+    isFollowed: false
+  },
+  {
+    id: 84,
+    name: 'user99',
+    avatar: 'https://i.imgur.com/fGVq8DU.jpeg',
+    account: 'user9',
+    followersCount: 3,
+    Followers: [
+      {
+        id: 64,
+        name: 'ＲＥＤ',
+        avatar: 'https://i.imgur.com/oEu32YO.jpeg',
+        account: 'asd',
+        Followship: {
+          id: 604,
+          followerId: 64,
+          followingId: 84,
+          createdAt: '2022-08-07T16:19:12.000Z',
+          updatedAt: '2022-08-07T16:19:12.000Z'
+        }
+      },
+      {
+        id: 164,
+        name: 'wjy',
+        avatar: 'https://i.imgur.com/1gLP1yi.jpeg',
+        account: 'wjy',
+        Followship: {
+          id: 724,
+          followerId: 164,
+          followingId: 84,
+          createdAt: '2022-08-10T10:18:14.000Z',
+          updatedAt: '2022-08-10T10:18:14.000Z'
+        }
+      },
+      {
+        id: 14,
+        name: 'Sandy',
+        avatar: 'https://i.imgur.com/ljf0Tla.jpeg',
+        account: 'user1',
+        Followship: {
+          id: 784,
+          followerId: 14,
+          followingId: 84,
+          createdAt: '2022-12-01T12:25:11.000Z',
+          updatedAt: '2022-12-01T12:25:11.000Z'
+        }
+      }
+    ],
+    isFollowed: true
+  },
+  {
+    id: 34,
+    name: 'user3',
+    avatar: 'https://randomuser.me/api/portraits/women/3.jpg',
+    account: 'user3',
+    followersCount: 2,
+    Followers: [
+      {
+        id: 84,
+        name: 'user99',
+        avatar: 'https://i.imgur.com/fGVq8DU.jpeg',
+        account: 'user9',
+        Followship: {
+          id: 324,
+          followerId: 84,
+          followingId: 34,
+          createdAt: '2022-08-07T14:04:41.000Z',
+          updatedAt: '2022-08-07T14:04:41.000Z'
+        }
+      },
+      {
+        id: 14,
+        name: 'Sandy',
+        avatar: 'https://i.imgur.com/ljf0Tla.jpeg',
+        account: 'user1',
+        Followship: {
+          id: 754,
+          followerId: 14,
+          followingId: 34,
+          createdAt: '2022-09-13T09:26:48.000Z',
+          updatedAt: '2022-09-13T09:26:48.000Z'
+        }
+      }
+    ],
+    isFollowed: true
+  },
+  {
+    id: 54,
+    name: 'user5',
+    avatar: 'https://randomuser.me/api/portraits/women/5.jpg',
+    account: 'user5',
+    followersCount: 2,
+    Followers: [
+      {
+        id: 14,
+        name: 'Sandy',
+        avatar: 'https://i.imgur.com/ljf0Tla.jpeg',
+        account: 'user1',
+        Followship: {
+          id: 4,
+          followerId: 14,
+          followingId: 54,
+          createdAt: '2022-08-07T13:00:41.000Z',
+          updatedAt: '2022-08-07T13:00:41.000Z'
+        }
+      },
+      {
+        id: 84,
+        name: 'user99',
+        avatar: 'https://i.imgur.com/fGVq8DU.jpeg',
+        account: 'user9',
+        Followship: {
+          id: 394,
+          followerId: 84,
+          followingId: 54,
+          createdAt: '2022-08-07T14:34:36.000Z',
+          updatedAt: '2022-08-07T14:34:36.000Z'
+        }
+      }
+    ],
+    isFollowed: true
+  },
+  {
+    id: 114,
+    name: '一拳超人',
+    avatar: 'https://i.imgur.com/IdzljwO.jpeg',
+    account: 'red2',
+    followersCount: 2,
+    Followers: [
+      {
+        id: 64,
+        name: 'ＲＥＤ',
+        avatar: 'https://i.imgur.com/oEu32YO.jpeg',
+        account: 'asd',
+        Followship: {
+          id: 534,
+          followerId: 64,
+          followingId: 114,
+          createdAt: '2022-08-07T16:17:30.000Z',
+          updatedAt: '2022-08-07T16:17:30.000Z'
+        }
+      },
+      {
+        id: 14,
+        name: 'Sandy',
+        avatar: 'https://i.imgur.com/ljf0Tla.jpeg',
+        account: 'user1',
+        Followship: {
+          id: 774,
+          followerId: 14,
+          followingId: 114,
+          createdAt: '2022-11-13T12:00:09.000Z',
+          updatedAt: '2022-11-13T12:00:09.000Z'
+        }
+      }
+    ],
+    isFollowed: true
+  },
+  {
+    id: 44,
+    name: 'user4',
+    avatar: 'https://randomuser.me/api/portraits/women/4.jpg',
+    account: 'user4',
+    followersCount: 1,
+    Followers: [
+      {
+        id: 84,
+        name: 'user99',
+        avatar: 'https://i.imgur.com/fGVq8DU.jpeg',
+        account: 'user9',
+        Followship: {
+          id: 334,
+          followerId: 84,
+          followingId: 44,
+          createdAt: '2022-08-07T14:04:42.000Z',
+          updatedAt: '2022-08-07T14:04:42.000Z'
+        }
+      }
+    ],
+    isFollowed: false
+  },
+  {
+    id: 64,
+    name: 'ＲＥＤ',
+    avatar: 'https://i.imgur.com/oEu32YO.jpeg',
+    account: 'asd',
+    followersCount: 1,
+    Followers: [
+      {
+        id: 14,
+        name: 'Sandy',
+        avatar: 'https://i.imgur.com/ljf0Tla.jpeg',
+        account: 'user1',
+        Followship: {
+          id: 484,
+          followerId: 14,
+          followingId: 64,
+          createdAt: '2022-08-07T16:03:20.000Z',
+          updatedAt: '2022-08-07T16:03:20.000Z'
+        }
+      }
+    ],
+    isFollowed: true
+  },
+  {
+    id: 94,
+    name: 'red1',
+    avatar: null,
+    account: 'red1',
+    followersCount: 0,
+    Followers: [],
+    isFollowed: false
+  },
+  {
+    id: 144,
+    name: 'red11',
+    avatar: null,
+    account: 'red11',
+    followersCount: 0,
+    Followers: [],
+    isFollowed: false
+  }
+]
+export default {
+  data () {
+    return {
+      users: []
+    }
+  },
+  created () {
+    this.fetchUsers()
+  },
+  methods: {
+    fetchUsers () {
+      this.users = dummyUsers.map(user => ({
+        id: user.id,
+        name: user.name,
+        avatar: user.avatar,
+        account: user.account,
+        isFollowed: user.isFollowed
+      }))
+    },
+    addFollow (id) {
+      this.users = this.users.map(user => {
+        if (user.id === id) {
+          return {
+            ...user,
+            isFollowed: true
+          }
+        }
+        return user
+      })
+    },
+    removeFollow (id) {
+      this.users = this.users.map(user => {
+        if (user.id === id) {
+          return {
+            ...user,
+            isFollowed: false
+          }
+        }
+        return user
+      })
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -93,7 +471,10 @@ export default {}
     height: 50px;
     margin-right: 8px;
     img {
-      width: 100%;
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      object-fit: cover;
       vertical-align: middle;
     }
   }
@@ -101,7 +482,7 @@ export default {}
     font-size: 16px;
     font-weight: 700;
     color: $black;
-    margin-bottom: 3px;
+    margin-bottom: 8px;
   }
   p {
     font-size: 14px;
