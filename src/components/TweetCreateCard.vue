989c704106f7084d59b8ff9cc8da4544c79d8582
JasonChou <swchou524@gmail.com>
2022-12-03 21:57:17 +0800

    feat: add component TweetCreateCard

--- src/components/TweetCreateCard.vue
+++ src/components/TweetCreateCard.vue
@@ -1,13 +1,148 @@
 <template>
-  <h1>TweetCreateCard</h1>
+  <div class="create-tweet-card">
+    <div class="card-body">
+      <a class="avatar">
+        <img src="~@/assets/image/tweet-default.png" alt="" />
+      </a>
+      <textarea
+        v-model="text"
+        class="text"
+        name=""
+        id=""
+        placeholder="有什麼新鮮事？"
+        @input="textValidator"
+      >
+      </textarea>
+    </div>
+    <div class="card-footer">
+      <button :disabled="isBtnDisabled" @click="handleButtonClick">推文</button>
+      <span :class="{ errorMessage: isBtnDisabled }">
+        {{ textLength }}/140
+      </span>
+      <span v-show="textOverflow" class="errorMessage"
+        >推文不得超過 140 字</span
+      >
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import { dayjs } from '@/utils/helpers'
 
+export default {
+  props: {
+    tweets: {
+      type: Array,
+      require: true
+    },
+    currentUser: {
+      type: Object,
+      require: true
+    }
+  },
+  data() {
+    return {
+      text: '',
+      isBtnDisabled: true,
+      textOverflow: false
+    }
+  },
+  computed: {
+    textLength() {
+      return this.text.length
+    }
+  },
+  methods: {
+    handleButtonClick() {
+      // TODO: API POST tweets
+      // 暫時建立的 dummyTweet
+      const newTweet = {
+        id: dayjs().valueOf(),
+        description: this.text,
+        createdAt: dayjs().toISOString(),
+        User: this.currentUser,
+        isLiked: false,
+        likeCount: 0,
+        replyCount: 0
+      }
+      // TODO: 發送事件到父層新增資料 將 newTweet 傳出
+      this.$emit('afterCreateTweet', newTweet)
+      this.text = ''
+      this.isBtnDisabled = true
+    },
+    textValidator() {
+      console.log(this.textLength)
+      if (this.textLength <= 0) {
+        this.isBtnDisabled = true
+        return
+      } else if (this.textLength > 140) {
+        this.isBtnDisabled = true
+        this.textOverflow = true
+        return
+      }
+      this.isBtnDisabled = false
+      this.textOverflow = false
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.create-tweet-card {
+  padding: 16px 25px;
+  border-left: 1px solid $light-blue2;
+  border-right: 1px solid $light-blue2;
+  border-bottom: 10px solid $light-blue2;
+}
+.card-body {
+  display: flex;
+  .avatar {
+    margin-right: 8px;
+  }
+  img {
+    width: 50px;
+    height: 50px;
+  }
+  .text {
+    height: 150px;
+    width: 100%;
+    padding-top: 12px;
+    border: 0;
+    resize: none;
+    font-size: 18px;
+    vertical-align: bottom;
+    &::placeholder {
+      font-size: 18px;
+      font-weight: 700;
+    }
+    &:focus {
+      outline: none;
+      border: 0;
+    }
+  }
+}
+.card-footer {
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  button {
+    padding: 8px 16px;
+    border-radius: 50px;
+    font-size: 16px;
+    cursor: pointer;
+    border: 0;
+    background-color: $brand-color;
+    color: #fff;
+    &:disabled {
+      background-color: $gray1;
+      cursor: default;
+    }
+  }
+  span {
+    margin-right: 10px;
+    &.errorMessage {
+      color: $Error;
+    }
+  }
+}
 </style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -6,20 +6,11 @@
         <div class="title">
           <h4>首頁</h4>
         </div>
-        <div class="create-tweet-card">
-          <div class="card-body">
-            <a class="avatar">
-              <img src="~@/assets/image/tweet-default.png" alt="" />
-            </a>
-            <textarea class="text" name="" id="" placeholder="有什麼新鮮事？">
-            </textarea>
-          </div>
-          <div class="card-footer">
-            <button>推文</button>
-            <span>10/140</span>
-            <span>推文不得超過 140 字</span>
-          </div>
-        </div>
+        <tweet-create-card
+          :currentUser="currentUser"
+          :tweets="tweets"
+          @afterCreateTweet="afterCreateTweet"
+        />
         <tweet-card
           v-for="tweet in tweets"
           :key="tweet.id"
@@ -37,6 +28,7 @@
 import NavTabs from '../components/NavTabs.vue'
 import UserPopular from '../components/UserPopular.vue'
 import TweetCard from '../components/TweetCard.vue'
+import TweetCreateCard from '../components/TweetCreateCard.vue'
 
 const dummyTweets = [
   {
@@ -1635,17 +1627,28 @@ const dummyTweets = [
     isLiked: false
   }
 ]
+const dummyUser = {
+  id: 5,
+  account: 'user4',
+  name: 'user4',
+  avatar: null,
+  introduction: 'Et voluptates alias eligendi perspiciatis.',
+  role: 'user',
+  front_cover: null
+}
 
 export default {
   name: 'Main',
   components: {
     NavTabs,
     UserPopular,
-    TweetCard
+    TweetCard,
+    TweetCreateCard
   },
   data() {
     return {
-      tweets: []
+      tweets: [],
+      currentUser: { ...dummyUser }
     }
   },
   created() {
@@ -1653,6 +1656,7 @@ export default {
   },
   methods: {
     fetchTweets() {
+      // TODO: API GET tweets
       this.tweets = dummyTweets.map((tweet) => ({
         id: tweet.id,
         description: tweet.description,
@@ -1686,6 +1690,9 @@ export default {
         }
         return tweet
       })
+    },
+    afterCreateTweet(newTweet) {
+      this.tweets.unshift(newTweet)
     }
   }
 }
@@ -1716,56 +1723,6 @@ export default {
     font-weight: 700;
     position: sticky;
     top: 0;
-  }
-  .create-tweet-card {
-    padding: 16px 25px;
-    border-left: 1px solid $light-blue2;
-    border-right: 1px solid $light-blue2;
-    border-bottom: 10px solid $light-blue2;
-  }
-}
-.card-body {
-  display: flex;
-  .avatar {
-    margin-right: 8px;
-  }
-  img {
-    width: 50px;
-    height: 50px;
-  }
-  .text {
-    height: 150px;
-    width: 100%;
-    padding-top: 12px;
-    border: 0;
-    resize: none;
-    font-size: 18px;
-    vertical-align: bottom;
-    &::placeholder {
-      font-size: 18px;
-      font-weight: 700;
-    }
-    &:focus {
-      outline: none;
-      border: 0;
-    }
-  }
-}
-.card-footer {
-  display: flex;
-  flex-direction: row-reverse;
-  align-items: center;
-  button {
-    padding: 8px 16px;
-    border-radius: 50px;
-    font-size: 16px;
-    cursor: pointer;
-    border: 0;
-    background-color: $brand-color;
-    color: #fff;
-  }
-  span {
-    margin-right: 10px;
   }
 }
 </style>
