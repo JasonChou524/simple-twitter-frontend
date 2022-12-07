d95f0f74fab38801b49522bce996b3f6a4f9544c
JasonChou <swchou524@gmail.com>
2022-12-07 22:44:51 +0800

    chore: 更新 dayjs 至 1.11.7 並設定為繁中語系

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,10 @@
 import dayjs from 'dayjs'
+import 'dayjs/locale/zh-tw'
+import localizedFormat from 'dayjs/plugin/localizedFormat'
 import relativeTime from 'dayjs/plugin/relativeTime'
 
 dayjs.extend(relativeTime)
+dayjs.extend(localizedFormat)
+dayjs.locale('zh-tw')
 
 export { dayjs }
