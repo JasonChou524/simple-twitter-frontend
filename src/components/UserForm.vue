4c2d41bde8bedef5387bc6cf97a18a1910f53d26
JasonChou <swchou524@gmail.com>
2022-12-13 23:38:33 +0800

    feat: UserEdit, UserForm 切版 v-model 連接表單

---
+++ src/components/UserForm.vue
@@ -0,0 +1,157 @@
+<template>
+  <form action="">
+    <ul>
+      <li class="input-area account">
+        <label for="account">帳號</label>
+        <input
+          v-model="user.account"
+          type="text"
+          id="account"
+          name="account"
+          placeholder="請輸入帳號"
+          required
+        />
+      </li>
+      <li class="input-area name">
+        <label for="name">使用者名稱</label>
+        <input
+          v-model="user.name"
+          type="text"
+          id="name"
+          name="name"
+          placeholder="請輸入使用者名稱"
+          required
+        />
+      </li>
+      <li class="input-area email">
+        <label for="email">Email</label>
+        <input
+          v-model="user.email"
+          type="text"
+          id="email"
+          name="email"
+          placeholder="請輸入 Email"
+          required
+        />
+      </li>
+      <li class="input-area password">
+        <label for="password">密碼</label>
+        <input
+          v-model="user.password"
+          type="password"
+          id="password"
+          name="password"
+          placeholder="請輸入密碼"
+          required
+        />
+      </li>
+      <li class="input-area checkPassword">
+        <label for="password">密碼確認</label>
+        <input
+          v-model="user.checkPassword"
+          type="password"
+          id="checkPassword"
+          name="checkPassword"
+          placeholder="請再次輸入密碼"
+          required
+        />
+      </li>
+    </ul>
+    <div class="btn">
+      <button v-if="user.isEdit" class="edit-btn">儲存</button>
+      <button v-else class="signup-btn">註冊</button>
+    </div>
+  </form>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data() {
+    return {
+      user: {
+        id: -1,
+        name: '',
+        account: '',
+        email: '',
+        password: '',
+        checkPassword: '',
+        isEdit: false
+      }
+    }
+  },
+  computed: {
+    ...mapState(['currentUser'])
+  },
+  created() {
+    this.fetchUser()
+  },
+  methods: {
+    fetchUser() {
+      if (this.currentUser.id) {
+        this.user = {
+          ...this.currentUser,
+          password: '',
+          checkPassword: '',
+          isEdit: true
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.input-area {
+  width: 100%;
+  height: 54px;
+  margin-bottom: 32px;
+  padding: 2px 10px;
+  border-bottom: 2px solid $gray1;
+  border-radius: 0px 0px 4px 4px;
+  background-color: $gray-white3;
+  display: flex;
+  flex-direction: column;
+  label {
+    color: $gray1;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 22px;
+  }
+  input {
+    height: 100%;
+    background-color: $gray-white3;
+    border: 0;
+    color: $black;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 26px;
+    &:focus {
+      outline: none;
+    }
+    &::placeholder {
+      color: $gray3;
+    }
+  }
+}
+.btn {
+  display: flex;
+  flex-direction: row-reverse;
+  button {
+    width: 100%;
+    height: 45px;
+    padding: 8px 24px;
+    background-color: $brand-color;
+    border: 0;
+    border-radius: 50px;
+    color: #fff;
+    font-size: 20px;
+    font-weight: 400;
+    cursor: pointer;
+  }
+  .edit-btn {
+    width: 88px;
+  }
+}
+</style>
--- src/views/UserEdit.vue
+++ src/views/UserEdit.vue
@@ -1,13 +1,62 @@
 <template>
-  <h1>UserEdit</h1>
+  <div class="edit-container">
+    <div class="row">
+      <nav-tabs class="col-3" />
+      <section class="col-6">
+        <div class="edit">
+          <div class="title">
+            <h4>帳戶設定</h4>
+          </div>
+          <user-form />
+        </div>
+      </section>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import NavTabs from '@/components/NavTabs.vue'
+import UserForm from '@/components/UserForm.vue'
 
+export default {
+  components: {
+    NavTabs,
+    UserForm
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.edit-container {
+  width: 100vw;
+  height: 100vh;
+  max-width: 1400px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.edit {
+  height: 100vh;
+  position: relative;
+  overflow-y: scroll;
+  border-right: 1px solid $light-blue2;
+  border-left: 1px solid $light-blue2;
+  &::-webkit-scrollbar {
+    width: 0;
+  }
+  .title {
+    padding: 24px;
+    border-bottom: 1px solid $light-blue2;
+    color: $black;
+    font-size: 24px;
+    font-weight: 700;
+    line-height: 26px;
+  }
+  form {
+    margin: 20px auto;
+    padding: 24px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+}
 </style>
