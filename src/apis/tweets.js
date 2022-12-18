ef520121a18d305421216eead0b0d546e3c27a5b
JasonChou <swchou524@gmail.com>
2022-12-18 16:37:27 +0800

    feat: Main getTweets API

---
+++ src/apis/tweets.js
@@ -0,0 +1,19 @@
+import { apiHelper } from './../utils/helpers'
+const getToken = () => localStorage.getItem('token')
+
+export default {
+  getTweets() {
+    return apiHelper.get('/tweets', {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
+  createTweet({ description }) {
+    return apiHelper.post(
+      '/tweets',
+      { description },
+      {
+        headers: { Authorization: `Bearer ${getToken()}` }
+      }
+    )
+  }
+}
